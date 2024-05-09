f44c2246ec134849d7d714595d0e1cb964cbf142
황상윤 <s_infinite@naver.com>
2024-05-10 04:23:56 +0900

    feat: post data props로 임시 분배

--- src/pages/posts/posts.page.tsx
+++ src/pages/posts/posts.page.tsx
@@ -1,5 +1,18 @@
-export const PostsPage = () => {
-	return <div />;
+import type { Frontmatter, Post } from "@/src/entities/posts/model/post.type";
+import { PostTitle } from "@/src/entities/posts/ui/post-title";
+import { MdxRemote } from "@/src/shared/common-ui/mdx";
+
+interface PostProps {
+	post: Post & Frontmatter;
+}
+
+export const PostsPage = ({ post }: PostProps) => {
+	return (
+		<>
+			<PostTitle title={post.title} />
+			<MdxRemote source={post.content} />
+		</>
+	);
 };
 
-export default PostsPage
+export default PostsPage;
