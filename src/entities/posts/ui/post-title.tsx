caac1e13c762bf43fed3926e950b7ab5340a135c
황상윤 <s_infinite@naver.com>
2024-05-10 03:55:37 +0900

    feat: post-title 구현

--- src/entities/posts/ui/post-title.tsx
+++ src/entities/posts/ui/post-title.tsx
@@ -1,3 +1,7 @@
-export const PostTitle = () => {
-	return null;
+interface PostTitleProps {
+	title: string;
+}
+
+export const PostTitle = ({ title }: PostTitleProps) => {
+	return <h1 className="text-2640 font-semibold w-full">{title}</h1>;
 };
