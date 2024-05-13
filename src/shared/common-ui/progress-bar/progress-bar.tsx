a8d7e5adc0fc0b162d2a07ddf15f17dda29b409e
황상윤 <s_infinite@naver.com>
2024-05-13 20:38:13 +0900

    feat: scroll-progress-bar 추가

--- src/shared/common-ui/progress-bar/progress-bar.tsx
+++ src/shared/common-ui/progress-bar/progress-bar.tsx
@@ -5,5 +5,11 @@ import { useScrollYPercent } from "@/src/shared/hooks/use-scroll-y-percent";
 export const ProgrresBar = () => {
 	const scrollYPercent = useScrollYPercent();
 
-	return <div className="fixed">{scrollYPercent}</div>;
+	return (
+		<div className="fixed w-full bg-seo-200 h-2.5">
+			<div className="bg-seo-600 h-2.5" style={{ width: `${scrollYPercent}%` }}>
+				{""}
+			</div>
+		</div>
+	);
 };
