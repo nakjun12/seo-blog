811a72e71b2f8dce7ad4c8c82f9f095f3e8a6162
황상윤 <s_infinite@naver.com>
2024-05-22 23:52:02 +0900

    feat: post-card 레이아웃 작성

--- src/_pages/Test/test.page.tsx
+++ src/_pages/Test/test.page.tsx
@@ -1,15 +1,17 @@
+import { PostCard } from "@/src/entities/posts/ui/post-card";
 import { ProgrresBar } from "@/src/shared/common-ui/progress-bar";
 
 const TestPage = () => {
 	return (
 		<>
-			<ProgrresBar />
+			{/* <ProgrresBar />
 			<p className="text-seo-200 font-regular text-sm w-1/2 xs:bg-sky-300 sm:bg-seo-500 md:bg-red-400 lg:bg-white xl:bg-yellow-200">
 				tailwind 설정 테스트
 			</p>
 			<div className="bg-seo-300 h-screen"> </div>
 			<div className="bg-seo-300 h-screen"> </div>
-			<div className="bg-seo-300 h-screen"> </div>
+			<div className="bg-seo-300 h-screen"> </div> */}
+			<PostCard />
 		</>
 	);
 };
--- src/entities/posts/ui/post-card.tsx
+++ src/entities/posts/ui/post-card.tsx
@@ -1,3 +1,30 @@
+import Image from "next/image";
+
+const data = {
+	title: "타이틀",
+	src: "",
+	description:
+		"타인의 범죄행위로 인하여 생명·신체에 대한 피해를 받은 국민은 법률이 정하는 바에 의하여 국가로부터 ...",
+	author: "김명진",
+	time: "2024-05-09T16:28:45Z",
+};
+
 export const PostCard = () => {
-	return null;
+	return (
+		<article className="">
+			<header className="">
+				<Image src="/test_bg.webp" alt="test" width={288} height={150} />
+				<h2 className="">{data.title}</h2>
+			</header>
+			<section className="">
+				<p>{data.description}</p>
+			</section>
+			<footer className="">
+				<time dateTime={data.time} className="">
+					{data.time}
+				</time>
+				<span className="">{data.author}</span>
+			</footer>
+		</article>
+	);
 };
