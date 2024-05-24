04246c12457c7129ec8860e45aad1f2d25f6df51
황상윤 <s_infinite@naver.com>
2024-05-25 07:40:05 +0900

    feat: header 추가

--- src/shared/common-ui/progress-bar/progress-bar.tsx
+++ src/shared/common-ui/progress-bar/progress-bar.tsx
@@ -6,8 +6,8 @@ export const ProgrresBar = () => {
 	const scrollYPercent = useScrollYPercent();
 
 	return (
-		<div className="fixed w-full bg-seo-200 h-2.5">
-			<div className="bg-seo-600 h-2.5" style={{ width: `${scrollYPercent}%` }}>
+		<div className="fixed w-full lg:max-w-6xl bg-seo-100 h-2.5">
+			<div className="bg-seo-600 h-2" style={{ width: `${scrollYPercent}%` }}>
 				{""}
 			</div>
 		</div>
--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { useScrollDirection } from "@/src/shared/hooks/use-scroll-direction";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export const Header = () => {
-	return null;
+	const scrollDirection = useScrollDirection();
+	const [isNav, setIsNav] = useState(true);
+	const path = usePathname();
+
+	useEffect(() => {
+		if (scrollDirection === "up") {
+			setIsNav(true);
+		} else {
+			setIsNav(false);
+		}
+	}, [scrollDirection]);
+
+	return (
+		<header className="top-0 z-50 mx-auto lg:max-w-6xl h-14  sticky">
+			{isNav && (
+				<nav className="h-full p-4 border-b bg-seo-100">
+					<Link
+						href="/"
+						title="DevDive"
+						className="text-1218 text-seo-100 bg-seo-600 rounded-full px-2 py-1.5"
+						onClick={(e) => {
+							if (path === "/") {
+								e.preventDefault();
+							}
+						}}
+					>
+						DevDive
+					</Link>
+				</nav>
+			)}
+		</header>
+	);
 };
