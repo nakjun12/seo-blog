8fdae0881752e10f013652949e1aeae9fda653f5
황상윤 <s_infinite@naver.com>
2024-05-25 05:03:47 +0900

    fix: breadcrumb 현재 위치 링크로 변경

--- src/shared/common-ui/breadcrumb/breadcrumb.tsx
+++ src/shared/common-ui/breadcrumb/breadcrumb.tsx
@@ -38,11 +38,17 @@ export const Breadcrumb = () => {
 			{breadcrumbs.map(({ label, url, isLast }, index) => (
 				<Fragment key={label}>
 					{url ? (
-						<Link href={url} title={url}>
+						<Link href={url} title={label}>
 							{label}
 						</Link>
 					) : (
-						<p className="font-bold text-seo-600">{label}</p>
+						<Link
+							href={label}
+							onClick={(e) => e.preventDefault()}
+							className="font-bold text-seo-600"
+						>
+							{label}
+						</Link>
 					)}
 					{!isLast && <span>/</span>}
 				</Fragment>
