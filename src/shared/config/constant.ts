e7a769024decef990b98b011d6feb88b9a527bb0
황상윤 <s_infinite@naver.com>
2024-05-25 04:53:44 +0900

    fix: BASE_URL 수정

--- src/shared/config/constant.ts
+++ src/shared/config/constant.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://seo-blog-murex";
+export const BASE_URL = "https://www.devdive.co.kr/";
 export const SITE_NAME = "DevDive";
 export const GOOGLE_TAG_MANAGER_ID = "GTM-PRDXHX4C";
 export const GOOGLE_ANALYTICS_ID = "G-1MEMCRD60Z";
