eff7737c416bdc96436a04d136c209dc83d09b7e
황상윤 <s_infinite@naver.com>
2024-05-25 11:05:02 +0900

    feat: 메인 페이지 메타데이터 임시 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import GoogleAddon from "@/src/app/google-addon";
 import NoscriptTagManager from "@/src/app/noscript-tagmanager";
 import { Providers } from "@/src/app/provider";
 import "@/src/app/style/tailwind.css";
-import { SITE_NAME } from "@/src/shared/config/constant";
+import { BASE_URL, SITE_NAME } from "@/src/shared/config/constant";
 import { Header } from "@/src/widgets/header";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
@@ -13,12 +13,35 @@ const pretendard = localFont({
 	display: "block",
 });
 
+const title = SITE_NAME;
+const description = "쉽게 읽는 개발 지식";
+
 export const metadata: Metadata = {
 	title: {
 		default: SITE_NAME,
 		template: `%s - ${SITE_NAME}`,
 	},
-	description: "쉽게 읽는 개발 지식",
+	description,
+	alternates: {
+		canonical: BASE_URL,
+	},
+	openGraph: {
+		title: SITE_NAME,
+		description,
+		url: BASE_URL,
+		type: "website",
+		siteName: `${SITE_NAME}`,
+	},
+	twitter: {
+		card: "summary",
+		site: BASE_URL,
+		title,
+		description,
+	},
+	robots: {
+		// todo: 나중에 포스팅시 isReleased로 치환
+		index: false,
+	},
 };
 
 export default function RootLayout({
