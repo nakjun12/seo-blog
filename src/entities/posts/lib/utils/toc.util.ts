6e59edc3b227f0c10c1593eab6c45050cc67176e
황상윤 <s_infinite@naver.com>
2024-06-05 23:24:38 +0900

    [feat] getScrollTop 유틸 함수 추가

--- src/entities/posts/lib/utils/toc.util.ts
+++ src/entities/posts/lib/utils/toc.util.ts
@@ -32,3 +32,11 @@ export const parseToc = (content: string): TocItem[] => {
 		}) || []
 	);
 };
+
+export const getScrollTop = () => {
+	if (!document.body) return 0;
+	const scrollTop = document.documentElement
+		? document.documentElement.scrollTop || document.body.scrollTop
+		: document.body.scrollTop;
+	return scrollTop;
+};
