0611792054936cc3b31c29302512098b12ce0486
황상윤 <s_infinite@naver.com>
2024-06-07 04:33:45 +0900

    [fix] toc 로직 오류 수정

--- src/shared/common-ui/toc/toc.tsx
+++ src/shared/common-ui/toc/toc.tsx
@@ -24,13 +24,13 @@ export const Toc = ({ toc }: TocProps) => {
 		if (!toc) return;
 
 		const scrollTop = getScrollTop();
-		const tocTops = toc.map(({ id }) => {
+		const tocTops = toc.map(({ id }, index) => {
 			const element = document.getElementById(id);
 			const top = element ? element.getBoundingClientRect().top + scrollTop : 0;
 
 			return {
 				id,
-				top,
+				top: index === 0 ? 0 : top,
 			};
 		});
 		setTocTops(tocTops);
@@ -81,6 +81,20 @@ export const Toc = ({ toc }: TocProps) => {
 
 	if (!toc || !tocTops) return null;
 
+	const handleLinkClick = (
+		e: React.MouseEvent<HTMLAnchorElement>,
+		id: string,
+	) => {
+		e.preventDefault();
+		const offset = 50; // 네브바의 높이 또는 원하는 오프셋 값
+		const element = document.getElementById(id);
+		if (element) {
+			const elementTop =
+				element.getBoundingClientRect().top + window.scrollY - offset;
+			window.scrollTo({ top: elementTop, behavior: "smooth" });
+		}
+	};
+
 	return (
 		<aside className="absolute -top-[200px] -mb-[100px] h-[calc(100%+150px)] left-full hidden xl:block">
 			<div className="sticky bottom-0 top-[200px] z-10 ml-[5rem] mt-[200px] w-[200px] overflow-y-auto overflow-x-hidden">
@@ -93,7 +107,11 @@ export const Toc = ({ toc }: TocProps) => {
 								activeId === item.id && "text-seo-600 font-semibold scale-105"
 							}`}
 						>
-							<Link href={`#${item.id}`} title={item.text}>
+							<Link
+								href={`#${item.id}`}
+								title={item.text}
+								onClick={(e) => handleLinkClick(e, item.id)}
+							>
 								{item.text}
 							</Link>
 						</li>
