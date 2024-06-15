444e96c34f8f53412496a27a9e9da76e2c2e4962
nakjun <111031253+nakjun12@users.noreply.github.com>
2024-06-15 16:16:23 +0900

    [fix] addon change (#93)

--- src/app/google-addon.tsx
+++ src/app/google-addon.tsx
@@ -23,6 +23,11 @@ export default function GoogleAddon() {
 				strategy="afterInteractive"
 				async
 			/>
+			<Script
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2439139777355926"
+				strategy="afterInteractive"
+			/>
 
 			{/* Google Analytics - Global site tag (gtag.js) */}
 			<Script
